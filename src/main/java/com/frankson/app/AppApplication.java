5306957f622ef69bde3ce4d567394276bf4108f8
fteotonho <frankson.teotonho@gmail.com>
2020-05-23 22:34:49 -0300

    Populating category table

--- src/main/java/com/frankson/app/AppApplication.java
+++ src/main/java/com/frankson/app/AppApplication.java
@@ -1,12 +1,29 @@
 package com.frankson.app;
 
+import com.frankson.app.model.Category;
+import com.frankson.app.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category category1 = new Category(null, "Informática");
+        Category category2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(category1, category2));
     }
 }
